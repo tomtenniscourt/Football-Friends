6247685c87a4f60e9653ba7deb20f834b752bab0
Jethro_Block <jethroblock22@gmail.COM>
2023-05-05 10:35:46 +0100

    Create admiredPlayer Schema
    PP:
    @Cootsy99

---
+++ Backend/Models/admiredPlayer.js
@@ -0,0 +1,13 @@
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+
+const admiredPlayerSchema = new Schema({
+    name: {type: String, required: true},
+    age: Number,
+    club: String,
+    reasonAdmired: {type: String, required: true}
+})
+
+const AdmiredPlayer = mongoose.model("AdmiredPlayer", admiredPlayerSchema)
+
+module.exports = AdmiredPlayer
