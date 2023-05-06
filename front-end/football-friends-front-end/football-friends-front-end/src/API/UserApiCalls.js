42bf41bfdd34a18cf0fc0f808292e385e49f19d9
Jethro_Block <jethroblock22@gmail.COM>
2023-05-06 17:15:01 +0100

    Add createUser API Call
    PP:
    @Cootsy99

--- front-end/football-friends-front-end/football-friends-front-end/src/API/UserApiCalls.js
+++ front-end/football-friends-front-end/football-friends-front-end/src/API/UserApiCalls.js
@@ -16,3 +16,18 @@ export const getOneUser = async(id) => {
         return err
   }
 }
+
+export const createUser = async(data) => {
+    try {
+        const result = await fetch(`${URL}/Register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        return result.json()
+    } catch(err) {
+return err
+    }
+}
--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Central/Homepage.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Central/Homepage.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import { getAllUsers, getOneUser } from "../../API/UserApiCalls";
+import { getAllUsers, getOneUser, createUser } from "../../API/UserApiCalls";
 
 export default function Homepage() {
 
   // const test = getAllUsers().then((res) => res.json())
   // console.log(test)
 
-const test2 = getOneUser("6454e712e12f643ac6218e6a"
-)
-console.log(test2)
+// const test2 = getOneUser("6454e712e12f643ac6218e6a")
+// console.log(test2)
+
+const test3 = createUser({
+  email: "test frontend email",
+  password: "frontend pass test"
+})
+console.log(test3)
 
   return <h1>Homepage</h1>;
 }
