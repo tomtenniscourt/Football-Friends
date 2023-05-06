3e7203d7b742eac8b0f9d9da9f40486a55534aa2
Jack Coots <jack.coots@newtoneurope.com>
2023-05-06 20:19:18 +0100

    Login component notes & Auth fix
    PP:
    @JayTeeBee

--- front-end/football-friends-front-end/football-friends-front-end/src/API/AuthenticationApiCalls.js
+++ front-end/football-friends-front-end/football-friends-front-end/src/API/AuthenticationApiCalls.js
@@ -1,14 +1,16 @@
+const URL = "http://localhost:5001";
+
 export const createUser = async (data) => {
-    try {
-        const result = await fetch(`${URL}/Register`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data)
-        })
-        return result.json()
-    } catch (err) {
-        return err
-    }
-}
+  try {
+    const result = await fetch(`${URL}/Register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return result.json();
+  } catch (err) {
+    return err;
+  }
+};
--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Login.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Login.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
 export default function Login() {
+  /** Jack to play with this component while working out authentication side of things */
+
   return <h1>Login</h1>;
 }
