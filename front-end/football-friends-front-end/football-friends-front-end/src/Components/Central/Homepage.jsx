019787290bd3fad52c2c47cd88cd2b31631a60eb
Jethro_Block <jethroblock22@gmail.COM>
2023-05-06 17:07:25 +0100

    Add getOneUser API Call
    PP:
    @Cootsy99

--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Central/Homepage.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Central/Homepage.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import { getAllUsers } from "../../API/UserApiCalls";
+import { getAllUsers, getOneUser } from "../../API/UserApiCalls";
 
 export default function Homepage() {
 
-  const test = getAllUsers().then((res) => res.json())
-  
-  console.log(test)
+  // const test = getAllUsers().then((res) => res.json())
+  // console.log(test)
+
+const test2 = getOneUser("6454e712e12f643ac6218e6a"
+)
+console.log(test2)
 
   return <h1>Homepage</h1>;
 }
