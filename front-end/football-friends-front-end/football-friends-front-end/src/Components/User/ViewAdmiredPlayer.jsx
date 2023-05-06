b46eaf4bb5dd8abe8b36ed2ac6005b479661b324
Jack Coots <jack.coots@newtoneurope.com>
2023-05-06 20:20:49 +0100

    ViewAdmiredPlayer Notes
    PP:
    @JayTeeBee

---
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/User/ViewAdmiredPlayer.jsx
@@ -0,0 +1,8 @@
+/**
+ * This component will represent a page/route in our web app. Users can navigate here
+ * by clicking on an 'AdmiredPlayerListItem' component from their 'Profile' page.
+ *
+ * It will display all of the same info from the 'AdmiredPlayerListItem' component
+ * except users will actually be able to edit the information on this page and
+ * update the backend accordingly
+ */
