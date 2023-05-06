1f62ee6dc5f4857c60cc4e094aaa25b8c4333212
Jack Coots <jack.coots@newtoneurope.com>
2023-05-06 20:21:47 +0100

    AllUsers component notes
    PP:
    @JayTeeBee

---
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/User/AllUsers.jsx
@@ -0,0 +1,8 @@
+/**
+ * The All users component should represent a page/route in our web app
+ * It should use the api to 'getAllUsers' and then render each of those users
+ * as a 'UserThumbnail' component.
+ *
+ * This component could also incorporate the 'search' bar at the top for example
+ *
+ */
