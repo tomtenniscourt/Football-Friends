58de7ea261cf30539fd709d8e44b14dc5a2c973b
Jack Coots <jack.coots@newtoneurope.com>
2023-05-06 15:57:33 +0100

    Neaten up file structure
    PP:
    @JayTeeBee

--- front-end/football-friends-front-end/football-friends-front-end/src/index.js
+++ front-end/football-friends-front-end/football-friends-front-end/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./Styling/index.css";
+import App from "./Components/Central/App";
+import reportWebVitals from "./Misc/reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
