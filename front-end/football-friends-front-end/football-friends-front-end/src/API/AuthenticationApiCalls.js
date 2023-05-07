cdbdf65e271c3ee06ee60f949f093047611f9d63
Jack Coots <jack.coots@newtoneurope.com>
2023-05-07 11:53:51 +0100

    Make login method to generate JWT

--- front-end/football-friends-front-end/football-friends-front-end/src/API/AuthenticationApiCalls.js
+++ front-end/football-friends-front-end/football-friends-front-end/src/API/AuthenticationApiCalls.js
@@ -1,5 +1,6 @@
 const URL = "http://localhost:5001";
 
+// Create a User when they Register
 export const createUser = async (data) => {
   try {
     const result = await fetch(`${URL}/Register`, {
@@ -14,3 +15,15 @@ export const createUser = async (data) => {
     return err;
   }
 };
+
+// Login
+export async function login(data) {
+  const response = await fetch(`${URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
