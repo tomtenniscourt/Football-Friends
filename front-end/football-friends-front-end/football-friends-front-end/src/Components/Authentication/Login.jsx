8f74174ce094cc83cc376b3ac94d983a85b32934
Tom_Court <tomcourt94@icloud.com>
2023-05-07 16:26:50 +0100

    copied login code

--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Login.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Login.jsx
@@ -2,5 +2,17 @@ import { useState } from "react";
 export default function Login() {
   /** Jack to play with this component while working out authentication side of things */
 
-  return <h1>Login</h1>;
+     return (
+    <div className="loginPage">
+      <h1>Login</h1>
+      <div className="loginForm">
+        <h2>test</h2>
+        <input className="inputUsername" type="text" />
+      <br></br>
+        <input className="inputPassword" type="text" />
+      <br></br>
+        <button className="searchButtonForFootballFriend">Submit</button>
+      </div>
+    </div>
+  );
 }
