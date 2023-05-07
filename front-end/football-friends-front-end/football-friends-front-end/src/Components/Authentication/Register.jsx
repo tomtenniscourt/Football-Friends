e43aa334b08d5e6e29cbcdf479dad9805ee4f364
Jack Coots <jack.coots@newtoneurope.com>
2023-05-07 11:38:03 +0100

    Flesh out Register component

--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Register.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Register.jsx
@@ -1,6 +1,47 @@
 import { useState } from "react";
+import { createUser } from "../../API/AuthenticationApiCalls";
+
 export default function Register() {
   /** Jack to play with this component while working out authentication side of things */
 
-  return <h1>Register</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    createUser({
+      email: email,
+      password: password,
+    }).then((result) => console.log("FROM BACKEND:   ", result));
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="text" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
 }
