b1b1298982985bcf9a8e221a43d3edcc95815325
Tom_Court <tomcourt94@icloud.com>
2023-05-08 20:56:00 +0100

    moved login and register nav bar links to right hand side of page

--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Register.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Register.jsx
@@ -10,67 +10,7 @@ export default function Register() {
     setSelectedOption(event.target.value);
   };
 
-  return (
-    <div className="registerPage">
-      <h1>Register</h1>
-      <div className="registerForm">
-        <h2>Submit your details below</h2>
-        <input
-          className="inputNewUsername"
-          type="text"
-          placeholder="Chosen Username"
-        />
-        <br />
-        <input
-          className="inputNewPassword"
-          type="text"
-          placeholder="Password"
-        />
-        <br />{" "}
-        <input
-          className="inputNewPasswordAgain"
-          type="text"
-          placeholder="Confirm Password"
-        />
-        <br />
-        <select
-          className="dropdown"
-          value={selectedOption}
-          onChange={handleOptionChange}
-        >
-          <option value="">Favourite Team</option>
-          <option value="Arsenal">Arsenal</option>
-          <option value="AstonVilla">Aston Villa</option>
-          <option value="Bournemouth">Bournemouth</option>
-          <option value="Brentford">Brentford</option>
-          <option value="Brighton&HoveAlbion">Brighton</option>
-          <option value="Chelsea">Chelsea</option>
-          <option value="CrystalPalace">Crystal Palace</option>
-          <option value="Everton">Everton</option>
-          <option value="LeedsUnited">Leeds United</option>
-          <option value="Fulham">Fulham</option>
-          <option value="LeicesterCity">Leicester City</option>
-          <option value="Liverpool">Liverpool</option>
-          <option value="ManchesterCity">Manchester City</option>
-          <option value="ManchesterUnited">Manchester United</option>
-          <option value="NewcastleUnited">Newcaste United</option>
-          <option value="NottinghamForest">Nottingham Forest</option>
-          <option value="Southampton">Southampton</option>
-          <option value="TottenhamHotspur">Tottenham Hotspurs</option>
-          <option value="WestHamUnited">Wankers United</option>
-          <option value="Wolves">Wolverhamton Wanderers</option>
-        </select>
-        <br />
-        <input
-          className="inputNewUsername"
-          type="text"
-          placeholder="Location Details"
-        />
-        <br />
-        <button className="registerButton">Submit</button>
-      </div>
-    </div>
-  );}
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -97,12 +37,33 @@ export default function Register() {
   };
 
   return (
-    <>
-      <h1>REGISTER PAGE</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="registerPage">
+      <h1>Register</h1>
+      <div className="registerForm">
+        <h2>Enter your details below and become a fooball friend!</h2>
+        <input
+          className="inputNewUsername"
+          type="text"
+          placeholder="Chosen Username"
+        />
+        <br />
+        <input
+          className="inputNewPassword"
+          type="text"
+          placeholder="Password"
+          value={password}
+
+        />
+        <br />{" "}
+        <input
+          className="inputNewPasswordAgain"
+          type="text"
+          placeholder="Confirm Password"
+          value={password}
+        />
+        <br />
         <label>
-          Email:
-          <input type="text" value={email} onChange={handleEmailChange} />
+        <input type="text" placeholder="Your Email" value={email} onChange={handleEmailChange} />
         </label>
         <label>
           Password:
@@ -112,7 +73,41 @@ export default function Register() {
             onChange={handlePasswordChange}
           />
         </label>
-        <button type="submit">Register</button>
-      </form>
-    </>
-  );
+        <select
+          className="dropdown"
+          value={selectedOption}
+          onChange={handleOptionChange}
+        >
+          <option value="">Favourite Team</option>
+          <option value="Arsenal">Arsenal</option>
+          <option value="AstonVilla">Aston Villa</option>
+          <option value="Bournemouth">Bournemouth</option>
+          <option value="Brentford">Brentford</option>
+          <option value="Brighton&HoveAlbion">Brighton</option>
+          <option value="Chelsea">Chelsea</option>
+          <option value="CrystalPalace">Crystal Palace</option>
+          <option value="Everton">Everton</option>
+          <option value="LeedsUnited">Leeds United</option>
+          <option value="Fulham">Fulham</option>
+          <option value="LeicesterCity">Leicester City</option>
+          <option value="Liverpool">Liverpool</option>
+          <option value="ManchesterCity">Manchester City</option>
+          <option value="ManchesterUnited">Manchester United</option>
+          <option value="NewcastleUnited">Newcaste United</option>
+          <option value="NottinghamForest">Nottingham Forest</option>
+          <option value="Southampton">Southampton</option>
+          <option value="TottenhamHotspur">Tottenham Hotspurs</option>
+          <option value="WestHamUnited">Wet Ham United</option>
+          <option value="Wolves">Wolverhamton Wanderers</option>
+        </select>
+        <br />
+        <input
+          className="inputNewUsername"
+          type="text"
+          placeholder="Location Details"
+        />
+        <br />
+        <button className="registerButton">Submit</button>
+      </div>
+    </div>
+  );}
--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Navigation/NavBar.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Navigation/NavBar.jsx
@@ -4,34 +4,37 @@ export default function NavBar() {
   /** Could do with some styling */
 
   return (
-    <nav style={{ fontSize: "18px" }}>
-      <Link to="/" style={{ color: "darkblue" }}>
-        Homepage
-      </Link>
-      &nbsp; | &nbsp;
-      <Link to="/profile" style={{ color: "darkblue" }}>
-        My Profile
-      </Link>
-      &nbsp; | &nbsp;
-      <Link to="/friends" style={{ color: "darkblue" }}>
-        My Football Friends
-      </Link>
-      &nbsp; | &nbsp;
-      <Link to="/messages" style={{ color: "darkblue" }}>
-        My Messages
-      </Link>
-      &nbsp; | &nbsp;
-      <Link to="/search" style={{ color: "darkblue" }}>
-        Search
-      </Link>
-      &nbsp; | &nbsp;
-      <Link to="/login" style={{ color: "darkblue" }}>
-        Log in/out
-      </Link>
-      &nbsp; | &nbsp;
-      <Link to="/register" style={{ color: "darkblue" }}>
-        Register
-      </Link>
+    <nav style={{ fontSize: "18px", display: "flex", justifyContent: "space-between" }}>
+      <div>
+        <Link to="/" style={{ color: "darkblue" }}>
+          Homepage
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/profile" style={{ color: "darkblue" }}>
+          My Profile
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/friends" style={{ color: "darkblue" }}>
+          My Football Friends
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/messages" style={{ color: "darkblue" }}>
+          My Messages
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/search" style={{ color: "darkblue" }}>
+          Search
+        </Link>
+      </div>
+      <div>
+        <Link to="/login" style={{ color: "darkblue" }}>
+          Log in/out
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/register" style={{ color: "darkblue" }}>
+          Register
+        </Link>
+      </div>
     </nav>
   );
 }
