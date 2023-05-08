b36f3c31666ed16622dd44707e3928952a3aa4d8
Tom_Court <tomcourt94@icloud.com>
2023-05-08 21:21:13 +0100

    Played around with  and added location dropdown menu

--- front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Register.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/Authentication/Register.jsx
@@ -4,15 +4,12 @@ import { createUser } from "../../API/AuthenticationApiCalls";
 export default function Register() {
   /** Jack to play with this component while working out authentication side of things */
 
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
-
-  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState("");
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -22,59 +19,63 @@ export default function Register() {
     setPassword(event.target.value);
   };
 
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleOptionChange = (event) => setSelectedOption(event.target.value);
+  const handleLocationChange = (event) => setSelectedLocation(event.target.value);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Email:", email);
     console.log("Password:", password);
+    console.log("Confirm Password:", confirmPassword);
+    console.log("Username:", username);
     createUser({
       email: email,
       password: password,
+      username: username,
+      confirmPassword: confirmPassword
     }).then((result) => {
       console.log("FROM BACKEND:   ", result);
     });
     setEmail("");
     setPassword("");
+    setConfirmPassword("");
+    setUsername("");
   };
+
 
   return (
     <div className="registerPage">
       <h1>Register</h1>
       <div className="registerForm">
         <h2>Enter your details below and become a fooball friend!</h2>
-        <input
-          className="inputNewUsername"
-          type="text"
-          placeholder="Chosen Username"
-        />
-        <br />
-        <input
-          className="inputNewPassword"
-          type="text"
-          placeholder="Password"
-          value={password}
-
-        />
-        <br />{" "}
-        <input
-          className="inputNewPasswordAgain"
-          type="text"
-          placeholder="Confirm Password"
-          value={password}
-        />
-        <br />
-        <label>
-        <input type="text" placeholder="Your Email" value={email} onChange={handleEmailChange} />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </label>
+               <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input type="text" value={email} onChange={handleEmailChange} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <label>
+            Confirm Password:
+            <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+          </label>
+          <label>
+            Username:
+            <input type="text" value={username} onChange={handleUsernameChange} />
+          </label>
+          <br />      
         <select
-          className="dropdown"
+          className="dropdownTeam"
           value={selectedOption}
           onChange={handleOptionChange}
         >
@@ -101,13 +102,59 @@ export default function Register() {
           <option value="Wolves">Wolverhamton Wanderers</option>
         </select>
         <br />
-        <input
-          className="inputNewUsername"
-          type="text"
-          placeholder="Location Details"
-        />
-        <br />
-        <button className="registerButton">Submit</button>
+        <select className="dropdownCounty" value={selectedLocation} onChange={handleLocationChange}>
+        <option value="">Select a county</option>
+        <option value="Avon">Avon</option>
+        <option value="Bedfordshire">Bedfordshire</option>
+        <option value="Berkshire">Berkshire</option>
+        <option value="Bristol">Bristol</option>
+        <option value="Buckinghamshire">Buckinghamshire</option>
+        <option value="Cambridgeshire">Cambridgeshire</option>
+        <option value="Cheshire">Cheshire</option>
+        <option value="Cleveland">Cleveland</option>
+        <option value="Cornwall">Cornwall</option>
+        <option value="Cumbria">Cumbria</option>
+        <option value="Derbyshire">Derbyshire</option>
+        <option value="Devon">Devon</option>
+        <option value="Dorset">Dorset</option>
+        <option value="Durham">Durham</option>
+        <option value="East Sussex">East Sussex</option>
+        <option value="Essex">Essex</option>
+        <option value="Gloucestershire">Gloucestershire</option>
+        <option value="Greater London">Greater London</option>
+        <option value="Greater Manchester">Greater Manchester</option>
+        <option value="Hampshire">Hampshire</option>
+        <option value="Hereford and Worcester">Hereford and Worcester</option>
+        <option value="Hertfordshire">Hertfordshire</option>
+        <option value="Humberside">Humberside</option>
+        <option value="Isle of Wight">Isle of Wight</option>
+        <option value="Kent">Kent</option>
+        <option value="Lancashire">Lancashire</option>
+        <option value="Leicestershire">Leicestershire</option>
+        <option value="Lincolnshire">Lincolnshire</option>
+        <option value="Merseyside">Merseyside</option>
+        <option value="Norfolk">Norfolk</option>
+        <option value="North Yorkshire">North Yorkshire</option>
+        <option value="Northamptonshire">Northamptonshire</option>
+        <option value="Northumberland">Northumberland</option>
+        <option value="Nottinghamshire">Nottinghamshire</option>
+        <option value="Oxfordshire">Oxfordshire</option>
+        <option value="Shropshire">Shropshire</option>
+        <option value="Somerset">Somerset</option>
+        <option value="South Yorkshire">South Yorkshire</option>
+        <option value="Staffordshire">Staffordshire</option>
+        <option value="Suffolk">Suffolk</option>
+        <option value="Surrey">Surrey</option>
+        <option value="Tyne and Wear">Tyne and Wear</option>
+        <option value="Warwickshire">Warwickshire</option>
+        <option value="West Midlands">West Midlands</option>
+        <option value="West Sussex">West Sussex</option>
+        <option value="West Yorkshire">West Yorkshire</option>
+        <option value="Wiltshire">Wiltshire</option>
+        </select>
+
+        <button className="registerButton" type="submit">Submit</button> 
+        </form>
       </div>
     </div>
   );}
