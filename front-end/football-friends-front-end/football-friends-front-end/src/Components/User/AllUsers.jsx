48198e9207f792019a87316a2ae6b167bf6a817d
Jethro_Block <jethroblock22@gmail.COM>
2023-05-10 15:04:53 +0100

    Add basic search functionality

--- front-end/football-friends-front-end/football-friends-front-end/src/Components/User/AllUsers.jsx
+++ front-end/football-friends-front-end/football-friends-front-end/src/Components/User/AllUsers.jsx
@@ -13,7 +13,11 @@ import { getAllUsers } from "../../API/UserApiCalls";
 
 export default function AllUsers() {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
   useEffect(() => {
     getAllUsers().then((output) => setUsers(output));
   }, []);
@@ -21,13 +25,47 @@ export default function AllUsers() {
   let ourUsers = <h3>No Users</h3>;
 
   if (users.length > 0) {
+    if (searchTerm) {
+      let usersToRender = users.filter((user) => {
+       return user.profileName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 
+      });
+      
+      ourUsers = usersToRender.map((user, index) => {
+        return <UserThumbnail user={user} key={user._id}></UserThumbnail>});
+
+    } else {
     ourUsers = users.map((user, index) => {
       return <UserThumbnail user={user} key={user._id}></UserThumbnail>;
-    });
+    })};
   }
   return (
     <>
       <h2>All Users</h2>
+
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search for users"
+                value={searchTerm}
+                onChange={handleInputChange}
+              />
+              <button
+                className="btn btn-primary"
+                type="button"
+                // onClick={handleSearch}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
       {ourUsers}
     </>
   );
