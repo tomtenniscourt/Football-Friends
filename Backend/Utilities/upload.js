a1a86fd5f3e6ec4cb309bada355d5782ecf99768
Tom_Court <tomcourt94@icloud.com>
2023-05-10 17:21:09 +0100

    bug fixes

--- Backend/Utilities/upload.js
+++ Backend/Utilities/upload.js
@@ -4,37 +4,38 @@ const path = require("path");
 // Setting up Disk Storage (as opposed to Cloud etc)
 // Destination for uploads will be the uploads folder
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads");
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
 
     // // Use this section if relative path causes issues
     //     const uploadsDir = path.join(__dirname, "../uploads");
     //     cb(null, uploadsDir);
     // },
-// Giving the uploaded file a unique name
+    // Giving the uploaded file a unique name
     filename: (req, file, cb) => {
-        const uniqueFileName = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, uniqueFileName + "-" + file.originalname)
-    },
+      const uniqueFileName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, uniqueFileName + "-" + file.originalname);
+    };
+  },
 });
 
 // Filtering out disallowed filetypes
 // mime = Multipurpose Internet Mail Extensions = filetype
 const fileFilter = (req, file, cb) => {
-    const allowedFileTypes = ["image/jpeg", "image/png", "image/gif"];
-    if (allowedFileTypes.includes(file.mimetype)) {
+  const allowedFileTypes = ["image/jpeg", "image/png", "image/gif"];
+  if (allowedFileTypes.includes(file.mimetype)) {
     // if the uploaded filetype is one of the allowed file types, pass true as the callback parameter, otherwise: else statement
-        cb(null, true);
-    } else {
-        cb(new Error("Invalid File Type"))
-    }
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid File Type"));
+  }
 };
 
 // Multer instance
 const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {fileSize: 5 * 1024 * 1024} // 5MB file size limit
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
 });
 
-module.exports = upload; 
+module.exports = upload;
